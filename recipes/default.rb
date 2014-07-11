75ddf4b5aeaa19ab7806d329762c255e4f36476d
osawayk <osawagiboy@gmail.com>
2014-07-11 18:35:00 +0900

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: users
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node["users"]["setup"].each do |usr|
+  users_manage usr["name"] do
+    group_id usr["group_id"]
+    action usr["action"]
+  end
+  if usr["sudo"].is_a?(Hash)
+    sudo usr["name"] do
+      %w{commands group nopasswd runas template user defaults variables}.each do |attr|
+        send(attr, usr["sudo"][attr]) if usr["sudo"][attr]
+      end
+    end
+  end
+end
