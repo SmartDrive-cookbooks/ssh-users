ae57e9c26734aa37b4afdd029686d9019b30d0af
osawagiboy <osawagiboy@gmail.com>
2014-07-28 02:54:13 +0900

    Updated recipe and attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default["ssh-users"]["setup"] = []
+default["ssh-users"]["default_data_bag"] = true
+default["ssh-users"]["data_bag_path"] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+data_bag_name = node["ssh-users"]["default_data_bag"] ? "users" : node["ssh-users"]["data_bag_path"]
+
 node["ssh-users"]["setup"].each do |usr|
   users_manage usr["name"] do
+    data_bag data_bag_name
     group_id usr["group_id"]
     action usr["action"]
   end
